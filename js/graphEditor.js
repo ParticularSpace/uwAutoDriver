c5f860902fd3967e545a5e3bcdd0a916513a5d19
Sam <samejones2018@gmail.com>
2023-12-19 21:27:40 -0800

    now can hover and select nodes

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -13,21 +13,31 @@ class GraphEditor {
 
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (evt) => {
-      const mouse = new Point(evt.offsetX, evt.offsetY);
-      this.hovered = getNearestPoint(mouse, this.graph.points);
-      if (this.hovered) {
-        this.selected = this.hovered;
-        return;
-      }
-      this.graph.addPoint(mouse);
-      this.selected = mouse;
-    });
+        const mouse = new Point(evt.offsetX, evt.offsetY);
+        this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+        if (this.hovered) {
+          this.selected = this.hovered;
+          return;
+        }
+        this.graph.addPoint(mouse);
+        this.selected = mouse;
+      });
+
+      this.canvas.addEventListener("mousemove", (evt) => {
+        const mouse = new Point(evt.offsetX, evt.offsetY);
+        this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+      });
   }
 
   display() {
     this.graph.draw(this.ctx);
+    if(this.hovered){
+        this.hovered.draw(this.ctx, { fill: true });
+      }
+
     if(this.selected){
       this.selected.draw(this.ctx, { outline: true });
     }
+    
   }
 }
