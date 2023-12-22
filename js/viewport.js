06bbef790ee636cfd2bcb449f09366ce30bcddd3
Sam <samejones2018@gmail.com>
2023-12-21 18:40:39 -0800

    updated viewport

--- js/viewport.js
+++ js/viewport.js
@@ -4,8 +4,8 @@ class Viewport {
     this.ctx = canvas.getContext("2d");
 
     this.zoom = 1;
-
-    this.offset = new Point(0, 0);
+    this.center = new Point(canvas.width / 2, canvas.height / 2);
+    this.offset = scale(this.center, -1);
 
     this.drag = {
       start: new Point(0, 0),
@@ -17,8 +17,26 @@ class Viewport {
     this.#addEventListeners();
   }
 
-  getMouse(evt) {
-    return new Point(evt.offsetX * this.zoom, evt.offsetY * this.zoom);
+  reset() {
+    this.ctx.restore();
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.save();
+    this.ctx.translate(this.center.x, this.center.y);
+    this.ctx.scale( 1 / this.zoom, 1 / this.zoom);
+    const offset = this.getOffset();
+    this.ctx.translate(offset.x, offset.y);
+  }
+
+  getMouse(evt, subtractDragOffset = false) {
+    const p = new Point(
+      (evt.offsetX - this.center.x) * this.zoom - this.offset.x,
+      (evt.offsetY - this.center.y) * this.zoom - this.offset.y
+    );
+    return subtractDragOffset ? subtract(p, this.drag.offset) : p;
+  }
+
+  getOffset() {
+    return add(this.offset, this.drag.offset);
   }
 
   #addEventListeners() {
@@ -46,17 +64,17 @@ class Viewport {
     }
   }
 
-    #handleMouseUp(evt) {
-        if(this.drag.active){
-            this.offset = add(this.offset, this.drag.offset);
-            this.drag = {
-                start: new Point(0, 0),
-                end: new Point(0, 0),
-                offset: new Point(0, 0),
-                active: false,
-              };
-        }
+  #handleMouseUp(evt) {
+    if (this.drag.active) {
+      this.offset = add(this.offset, this.drag.offset);
+      this.drag = {
+        start: new Point(0, 0),
+        end: new Point(0, 0),
+        offset: new Point(0, 0),
+        active: false,
+      };
     }
+  }
 
   #handleMouseWheel(evt) {
     const dir = Math.sign(evt.deltaY);
