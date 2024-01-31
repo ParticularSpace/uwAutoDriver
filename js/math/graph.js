17595eb576ac1689ea38687706d59d607254b404
Sam <samejones2018@gmail.com>
2024-01-30 17:43:04 -0800

    pushed

--- js/math/graph.js
+++ js/math/graph.js
@@ -1,90 +1,88 @@
 class Graph {
-    constructor(points = [], segments = []) {
-        this.points = points,
-        this.segments = segments;
-    }
+   constructor(points = [], segments = []) {
+      this.points = points;
+      this.segments = segments;
+   }
 
-    static load(info){
-        const points = info.points.map((i) => new Point(i.x, i.y));
-        const segments = info.segments.map((i) => new Segment(
-            points.find((p) => p.equals(i.p1)),
-            points.find((p) => p.equals(i.p2))
-        ));
-        return new Graph(points, segments);
-    }
-    
-    hash(){
-        return JSON.stringify(this);
-    }
-    
-    addPoint(point){
-        this.points.push(point);
-    }
+   static load(info) {
+      const points = info.points.map((i) => new Point(i.x, i.y));
+      const segments = info.segments.map((i) => new Segment(
+         points.find((p) => p.equals(i.p1)),
+         points.find((p) => p.equals(i.p2))
+      ));
+      return new Graph(points, segments);
+   }
 
-    containsPoint(point){
-        return this.points.find((p) => p.equals(point));
-    }
+   hash() {
+      return JSON.stringify(this);
+   }
 
-    tryAddPoint(point){
-        if(!this.containsPoint(point)){
-            this.addPoint(point);
-            return true;
-        }
-        return false;
-    }
+   addPoint(point) {
+      this.points.push(point);
+   }
 
-    removePoint(point){
-        const segs = this.getSegmentsWithPoint(point);
-        for (const seg of segs){
-            this.removeSegment(seg);
-        }
-        this.points.splice(this.points.indexOf(point), 1);
-    }
+   containsPoint(point) {
+      return this.points.find((p) => p.equals(point));
+   }
 
-    addSegment(seg){
-        this.segments.push(seg);
-    }
+   tryAddPoint(point) {
+      if (!this.containsPoint(point)) {
+         this.addPoint(point);
+         return true;
+      }
+      return false;
+   }
 
-    containsSegment(seg){
-        return this.segments.find((s) => s.equals(seg))
-    }
+   removePoint(point) {
+      const segs = this.getSegmentsWithPoint(point);
+      for (const seg of segs) {
+         this.removeSegment(seg);
+      }
+      this.points.splice(this.points.indexOf(point), 1);
+   }
 
-    tryAddSegment(seg){
-        if(!this.containsSegment(seg) && !seg.p1.equals(seg.p2)){
-            this.addSegment(seg);
-            return true;
-        }
-        return false;
-    }
+   addSegment(seg) {
+      this.segments.push(seg);
+   }
 
-    removeSegment(seg){
-        this.segments.splice(this.segments.indexOf(seg), 1);
-    }
+   containsSegment(seg) {
+      return this.segments.find((s) => s.equals(seg));
+   }
 
-    getSegmentsWithPoint(point){
-        const segs = [];
-        for (const seg of this.segments){
-            if(seg.includes(point)){
-                segs.push(seg);
-            }
-        }
-        return segs;
-    }
+   tryAddSegment(seg) {
+      if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+         this.addSegment(seg);
+         return true;
+      }
+      return false;
+   }
 
-    dispose(){
-        this.points.length = 0;
-        this.segments.length = 0;
-    }
+   removeSegment(seg) {
+      this.segments.splice(this.segments.indexOf(seg), 1);
+   }
 
- 
+   getSegmentsWithPoint(point) {
+      const segs = [];
+      for (const seg of this.segments) {
+         if (seg.includes(point)) {
+            segs.push(seg);
+         }
+      }
+      return segs;
+   }
 
-    draw(ctx){
-        for (const seg of this.segments){
-            seg.draw(ctx);
-        }
+   dispose() {
+      this.points.length = 0;
+      this.segments.length = 0;
+   }
 
-        for (const point of this.points){
-            point.draw(ctx);
-        }
-    }
+   draw(ctx) {
+      for (const seg of this.segments) {
+         seg.draw(ctx);
+      }
+
+      for (const point of this.points) {
+         point.draw(ctx);
+      }
+   }
 }
